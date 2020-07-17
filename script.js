c8010d85f61139a57b219dae055cea2f9b23a748
AndreaFedeli <andrea.fedeli159@gmail.com>
2020-07-17 15:08:26 +0200

    vers 2 da implementare

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.getElementById("genera").addEventListener("click", function () {
   var dati = document.getElementById("anag").value;
   document.getElementById("demo").innerHTML = dati;
-  document.getElementById("carrozza").innerHTML = numRandom(1,10);
+  var carrozza = Math.floor(Math.random() * 10);
+  document.getElementById("carrozza").innerHTML = carrozza;
+  var codice = Math.floor(Math.random() * (99999 - 90000 + 1)) + 90000;
+  document.getElementById("codice").innerHTML=codice;
 }
 );
